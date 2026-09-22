7568c4786737451e5ef743c502a82249621fed5e
Contractor-x <dada4ash@gmail.com>
2026-09-22 22:56:28 +0100

    added the vercel domain app to hthe allow_origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -43,6 +43,7 @@ app.add_middleware(
         "http://localhost:5500",
         "http://127.0.0.1:5500",
         "http://localhost:8000",
+        "https://signin-signup-system.vercel.app/",
         "http://127.0.0.1:8000",
         settings.FRONTEND_URL,
     ],
